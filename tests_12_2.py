56fab1e5338141938db81053d9a333487871e21d
hypezewq <quoiaaz@gmail.com>
2024-10-16 19:34:56 +0500

    Тесты...

--- tests_12_2.py
+++ tests_12_2.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class Runner:
     def __init__(self, name, speed=5):
         self.name = name
@@ -32,8 +35,47 @@ class Tournament:
             for participant in self.participants:
                 participant.run()
                 if participant.distance >= self.full_distance:
-                    finishers[place] = participant
+                    finishers[place] = participant.name
                     place += 1
                     self.participants.remove(participant)
 
         return finishers
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    @classmethod
+    def tearDownClass(cls):
+        for key, value in cls.all_results.items():
+            print(value)
+
+    def setUp(self):
+        self.all_results = {}
+        self.first_runner = Runner("Усэйн", 10)
+        self.second_runner = Runner("Андрей", 9)
+        self.third_runner = Runner("Ник", 3)
+
+    def test_tournament1(self):
+        tournament = Tournament(90, self.first_runner, self.third_runner)
+        self.all_results = tournament.start()
+        self.assertEqual(self.all_results[max(self.all_results)], "Ник")
+        TournamentTest.all_results["first"] = self.all_results
+
+    def test_tournament2(self):
+        tournament = Tournament(90, self.second_runner, self.third_runner)
+        self.all_results = tournament.start()
+        self.assertEqual(self.all_results[max(self.all_results)], "Ник")
+        TournamentTest.all_results["second"] = self.all_results
+
+    def test_tournament3(self):
+        tournament = Tournament(90, self.first_runner, self.second_runner, self.third_runner)
+        self.all_results = tournament.start()
+        self.assertEqual(self.all_results[max(self.all_results)], "Ник")
+        TournamentTest.all_results["third"] = self.all_results
+
+
+if __name__ == '__main__':
+    unittest.main()
